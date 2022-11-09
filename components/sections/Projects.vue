055930dbeccb3002a08fa7ad9be6acce6affefa3
Karzten <luiszegarrafernandez@gmail.com>
2022-11-09 10:51:08 -0500

    Add(Projects.vue): Add chips for filter projects

--- components/sections/Projects.vue
+++ components/sections/Projects.vue
@@ -1,6 +1,11 @@
 <template>
-  <section class="flex flex-col gap-6 font-oxanium px-5">
+  <section class="flex flex-col gap-8 font-oxanium px-5">
     <h1 class="text-center font-bold text-2xl lg:text-3xl">Mis proyectos</h1>
+    <div class="grid grid-cols-2 gap-3 sm:grid-cols-3 md:flex md:flex-row md:gap-4 justify-center text-xl -mt-2 ">
+      <button class="chips active">Todos</button>
+      <button class="chips">Diseño UX/UI</button>
+      <button class="chips">Frontend</button>
+    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <Card />
       <Card />
@@ -18,3 +23,27 @@ export default {
 }
 </script>
 
+<style scoped>
+  .chips {
+    @apply text-green-950 font-semibold bg-white py-2 px-4 rounded-md border border-green-950 text-sm;
+  }
+  .active{
+    @apply text-white font-semibold bg-green-950 py-2 px-4 rounded-md
+  }
+
+  @layer utilities{
+    .chips:hover {
+      @apply bg-green-950 text-white;
+    }
+    @screen sm {
+      .chips {
+        @apply text-base;
+      }
+    }
+    @screen md {
+      .chips {
+        @apply text-xl;
+      }
+    }
+  }
+</style>
