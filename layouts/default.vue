d23dbfb968ed58b814f1b2f38ebc3dbf00748d1c
Karzten <luiszegarrafernandez@gmail.com>
2022-11-09 19:34:18 -0500

    Refactor(index.vue): Put header in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <Header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Header from '~/components/UI/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 html {
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="">
-    <Header />
     <Presentation class="mt-8 md:mt-10 lg:mt-12"/>
     <Description class="mt-8 md:mt-10 lg:mt-12"/>
     <Skills class="mt-8 md:mt-10 lg:mt-12"/>
@@ -10,7 +9,7 @@
 </template>
 
 <script>
-import Header from '~/components/UI/Header.vue'
+
 import Presentation from '~/components/sections/Presentation.vue'
 import Description from '~/components/sections/Description.vue'
 import Skills from '~/components/sections/Skills.vue'
